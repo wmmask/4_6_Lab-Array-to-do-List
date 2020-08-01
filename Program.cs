6001ad0b22f688bb5548f18c76af646933966e05
wmmask <wf1029@post.com>
2020-07-31 19:45:51 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _4_6_Lab_Array_To_Do_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            // defining arrays
+            string[] days = { "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday" };
+            string[] tasks = new string[7];
+            // cycle through each day of week capturing daily tasks
+            for (int i=0; i<days.Length; i++)
+            { 
+                Console.WriteLine("Type in a new task for " + days[i]);
+                tasks[i] = Console.ReadLine();
+            }
+            // print our the weekly day-task list
+            for (int i = 0; i < days.Length; i++)
+            {
+                Console.WriteLine(days[i] +" : " + tasks[i]);
+            }
+        }
+    }
+}
